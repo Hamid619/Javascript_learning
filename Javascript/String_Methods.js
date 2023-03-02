8ca8398fbbd631375676e36655979c11851303d5
Hamid619 <hamidzaib07@gmail.con>
2023-03-02 23:50:32 +0500

    String Methods

---
+++ Javascript/String_Methods.js
@@ -0,0 +1,18 @@
+// let name = "Hamid";
+// let name2 = "Zeb";
+// let name3 = "    Khan"
+// console.log(name.length);
+// console.log(name.toLowerCase());
+// console.log(name.toUpperCase());
+// console.log(name.slice(2,4));
+// console.log(name.slice(2));
+// console.log(name.replace("Hamid", "Zaib"))
+// console.log(name.concat(" is a Friend of ",name2, " OK"));
+// console.log(name3);
+// console.log(name3.trim());
+
+let Name = "Hamid";
+for ( let i = 0; i<=Name.length; i++)
+{
+    console.log(Name[i]);
+}

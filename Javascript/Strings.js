d6ac49cfa411686339bbe9887661855b6d06fb18
Hamid619 <hamidzaib07@gmail.con>
2023-03-02 07:41:26 +0500

    Strings Lecture

---
+++ Javascript/Strings.js
@@ -0,0 +1,27 @@
+//String
+let name = "Hamid";
+console.log(name);
+console.log(name.length);
+let name2 = 'Zaib'
+console.log(name[0]);
+console.log(name[1]);
+console.log("--------------------------------");
+// Template Literals
+let boy1 = "Ali";
+let boy2 = "Khan";
+console.log(`${boy1} is a friend of ${boy2}`);
+console.log("--------------------------------");
+
+//Escape Sequence Characters
+let Fruit = 'App\'le';
+console.log(Fruit);
+let Fruit0 = "App\"le";
+console.log(Fruit0);
+let Fruit1 = 'App\nle';
+console.log(Fruit1);
+let Fruit2 = 'App\rle';
+console.log(Fruit2);
+let Fruit3 = 'App\tle';
+console.log(Fruit3);
+
+
